79bc3661029dc43d47c626c9810459ad2fcf9dcf
JJWizardMP <jj.mpwizard@gmail.com>
2022-01-13 18:50:41 -0500

    Setting url

--- core/settings.py
+++ core/settings.py
@@ -28,9 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1' ]
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1' 
+                 'https://django-server-booking.herokuapp.com']
 
 
 # Application definition
