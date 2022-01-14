751862706327b097c34fb423a2b5258bab32eeb1
JJWizardMP <jj.mpwizard@gmail.com>
2022-01-13 21:21:39 -0500

    add static directory

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['0.0.0.0', 'localhost', '127.0.0.1',
                  'https://django-server-booking.herokuapp.com']
