76db71e4bcc3411374ab3684d0084bc6c8cca3a9
JJWizardMP <jj.mpwizard@gmail.com>
2022-01-13 20:26:09 -0500

    changes settings

--- core/urls.py
+++ core/urls.py
@@ -16,10 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
+
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    path(r'', admin.site.urls),
+    path('admin/', admin.site.urls),
     path("graphql", csrf_exempt(GraphQLView.as_view(graphiql=True))),
-    path(r'^admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+]
